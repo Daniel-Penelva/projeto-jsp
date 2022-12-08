efd33175badf373aea41fd82fccd5010efcdbe1a
Daniel Penelva <d4n.andrade@gmail.com>
2022-12-08 20:37:45 -0300

    Fazendo simulação de login no sistema.

--- projeto-jsp/src/main/java/servlets/ServletLogin.java
+++ projeto-jsp/src/main/java/servlets/ServletLogin.java
@@ -37,16 +37,35 @@ public class ServletLogin extends HttpServlet {
 
 		/* Verificar se os valores login e senha foram preenchidos e validados */
 		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
-			
+
 			/* Esses valores sao atribuidos ao objeto modelLogin */
 			ModelLogin modelLogin = new ModelLogin();
 			modelLogin.setLogin(login);
 			modelLogin.setSenha(senha);
-		}else {
-			
-			/*Redireciona para outra tela caso o usuário não preencha o login e a senha*/
+
+			/* Simulando acesso do Login */
+			if (modelLogin.getLogin().equalsIgnoreCase("admin") 
+					&& modelLogin.getSenha().equalsIgnoreCase("admin")) {
+				
+				/* Atributo de sessão para manter o usuário logado */
+				request.getSession().setAttribute("usuario", modelLogin.getLogin());
+				
+				/* Acessando e redirecionando para a pagina principal */
+				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+				redirecionar.forward(request, response);
+
+			} else {
+				/* simulando preenchimento errado de login e senha */
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o login e a senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+
+		} else {
+
+			/* Redireciona para outra tela caso o usuário não preencha o login e a senha */
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
-			request.setAttribute("msg", "Informe o login e a senha corretamente!");
+			request.setAttribute("msg", "Informe o login e a senha!");
 			redirecionar.forward(request, response);
 		}
 	}
