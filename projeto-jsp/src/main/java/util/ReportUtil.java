dad087f326d3e491f66dc52ce58681ef0b091e8f
Daniel Penelva <d4n.andrade@gmail.com>
2023-02-14 02:25:33 -0300

    SubRelatorio com Jasper

--- projeto-jsp/src/main/java/util/ReportUtil.java
+++ projeto-jsp/src/main/java/util/ReportUtil.java
@@ -11,24 +11,46 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-
-@SuppressWarnings({"rawtypes","unchecked"})
-public class ReportUtil implements Serializable{
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class ReportUtil implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	public byte[] geraRelatorioPDF( List listaDados, String nomeRelatorio, ServletContext servletcontext) throws Exception{
-		
+	public byte[] geraRelatorioPDF(List listaDados, String nomeRelatorio, ServletContext servletcontext)
+			throws Exception {
+
 		/* Cria a lista de dados que vem do sql da consulta feita */
 		JRBeanCollectionDataSource jrbcds = new JRBeanCollectionDataSource(listaDados);
-		
-		/* Vamos passar o nome do package 'relatorio' 
-		 * File.separator é para separar as barras (depende do sistema operacional) */
+
+		/*
+		 * Vamos passar o nome do package 'relatorio' File.separator é para separar as
+		 * barras (depende do sistema operacional)
+		 */
 		String caminhoJasper = servletcontext.getRealPath("relatorio") + File.separator + nomeRelatorio + ".jasper";
-		
+
 		/* Carregar o jasper passando os dados para ele */
 		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, new HashMap(), jrbcds);
-		
+
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+	}
+
+	
+	/* Gerar Sub relatorio de telefone */
+	public byte[] geraRelatorioPDF(List listaDados, String nomeRelatorio, HashMap<String, Object> params,
+			ServletContext servletcontext) throws Exception {
+
+		/* Cria a lista de dados que vem do sql da consulta feita */
+		JRBeanCollectionDataSource jrbcds = new JRBeanCollectionDataSource(listaDados);
+
+		/*
+		 * Vamos passar o nome do package 'relatorio' File.separator é para separar as
+		 * barras (depende do sistema operacional)
+		 */
+		String caminhoJasper = servletcontext.getRealPath("relatorio") + File.separator + nomeRelatorio + ".jasper";
+
+		/* Carregar o jasper passando os dados para ele */
+		JasperPrint impressoraJasper = JasperFillManager.fillReport(caminhoJasper, params, jrbcds);
+
 		return JasperExportManager.exportReportToPdf(impressoraJasper);
 	}
 
